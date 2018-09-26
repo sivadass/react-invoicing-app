37d529d3a6ca437f2d15ac19323177692f07fb41
Sivadass N <nsivadass@gmail.com>
2018-09-27 00:47:39 +0530

    Connected graphql server

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,37 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const GET_USERS = gql`
+  {
+    users {
+      first_name
+      last_name
+      email
+      id
+    }
+  }
+`;
 
 const Home = () => {
   return (
-    <div className="app-wrapper">
-      <header className="container fluid">Home</header>
-      <article className="container">Welcome to React Invoicing App</article>
-    </div>
+    <Query query={GET_USERS}>
+      {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error! ${error.message}`;
+        return (
+          <div className="app-wrapper">
+            <header className="container fluid">Home</header>
+            <article className="container">
+              Welcome to React Invoicing App
+            </article>
+            {data.users.map(user => {
+              return <div>{user.email}</div>;
+            })}
+          </div>
+        );
+      }}
+    </Query>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import "./styles/index.scss";
 
@@ -8,19 +10,25 @@ import Header from "./components/common/header";
 import Sidebar from "./components/common/side-bar";
 import Home from "./components/pages/home";
 
+const client = new ApolloClient({
+  uri: "https://graphql-engine-playground.herokuapp.com/v1alpha1/graphql"
+});
+
 const App = props => {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <div className="app-container">
-          <Sidebar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="app">
+          <Header />
+          <div className="app-container">
+            <Sidebar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 };
 
